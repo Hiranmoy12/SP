0e710077352d2f4278adfde7c8f04d907f6700dc
Newly Creator <156579882+Hiranmoy12@users.noreply.github.com>
2026-04-28 14:55:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use('/static', express.static(path.join(__dirname, 'static')));
 // API endpoint
 app.get('/api/message', (req, res) => {
     res.json({
-        "greeting": "Hey IK you wont see this but still.....",
-        "message": "I just wanna say Goodbye to you . As you will be leaving the city by 30th April. So not at least *Am sorry for eveything if I have hurted you then forgive me.I didn't mean to hurt you ever. Ik am not the boy of your choice and I hope you will find a better boy then me . Be happy be enjoyable and I hope you will find your next patner soon .* Yesterday you told ... as soon I get a gf I should block you. But i won't block you as I don't wanna losse you fully . But if you gets a bf you can block me according to your wish . ",
+        "greeting": "Hey ik you won't see this but still.....",
+        "message": "I just wanna say Goodbye to you . As you will be leaving the city by 30th April. So not at least *Am sorry for eveything if I have hurted you then forgive me.I didn't mean to hurt you ever. Ik am not the boy of your choice and I hope you will find a better boy then me . Be happy be enjoyable and I hope you will find your next patner soon .* Yesterday you told ... as soon I get a gf I should block you. But i won't block you as I don't wanna losse you fully . But if you gets a bf you can block me according to your wish . Just don't trust any boy so easily , You can be mamipulated easily 😝. Take care of your health too. And find a patner who dosen't have any trust issues .",
         "wishes": "I wish you all the best for your future and I hope you achieve all your dreams. Have a safe trip!🌸✨",
         "signOff": "❤️"
     });
